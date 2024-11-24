2642f1a35a206a7b7613f24ddcc253732a51274e
elkarni bachar <diamobachar06@gmail.com>
2024-11-24 17:40:08 +0100

    bugfix(swagger): fix swagger-ui error -failed to load external configuration-

--- src/main/java/com/example/authservice/configs/security/OpenEndpointsConfig.java
+++ src/main/java/com/example/authservice/configs/security/OpenEndpointsConfig.java
@@ -1,13 +1,14 @@
 package com.example.authservice.configs.security;
 
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenEndpointsConfig {
   @Bean
   public List<String> permittedEndpoints() {
-    return List.of("/api/v1/auth/login", "/api/v1/auth/register", "/error");
+    return List.of("/api/v1/auth/login", "/api/v1/auth/register", "/error", "/api/v1/swagger-ui/**", "/api/v1/api-docs/**");
   }
 }
